58f68e04b320469e2056e622c032354f4765470e
KeerthiK2005 <keerthisree2005@gmail.com>
2023-12-24 16:21:53 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Keerthi Kuna",
+  title: "MLSA'23 | Aspiring SWE | JEE ADV'22 | EDC,GNITS",
+  email: "Kuna.Keerthi@studentambassadors.com",
+  gitHub: "https://github.com/KeerthiK2005",
+  instagram: "",
+  linkedIn: "www.linkedin.com/in/keerthi-kuna",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "@sree_kuna86213",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#C3ADC3";
+const secondaryColor = "#C3ADC3";
 
 const App = () => {
   return (
